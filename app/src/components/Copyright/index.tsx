7581d709d2ae97f9aa0c985ade4c2b33ba35e711
Nikita Sannikov <79974986+Nikita8Sannikov@users.noreply.github.com>
2024-11-01 20:39:47 +0600

    Messenger 21 (#3)
    * add Link component
    
    * add mui linkProps to type
    
    * replace <Link> tags with our component
    
    * fix comments
    
    * remove excess callback
    
    * add variant="body2" to LinkComponent props
    
    * fix "variant" props & delete log
    
    ---------
    
    Co-authored-by: Nikita8Sannikov <sannikovnikita@gmail.com>

--- app/src/components/Copyright/index.tsx
+++ app/src/components/Copyright/index.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Link from "@mui/material/Link";
+import LinkComponent from "../Common/Link";
 
 import { Pages } from "../../types/enums";
 
 import "./copyright.scss";
 
 export default React.memo(function Copyright() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return (
-        <div className="copyright">
-            Copyright ©
-            <Link href={Pages.aboutUs} variant="body2" onClick={() => navigate(Pages.aboutUs)}>
-                ВК-КЛОН
-            </Link>
-            {" "}{new Date().getFullYear()}.
-        </div>
-    );
+	return (
+		<div className="copyright">
+			Copyright ©
+			<LinkComponent
+				href={Pages.aboutUs}
+				variant="body2"
+				onClick={() => navigate(Pages.aboutUs)}
+			>
+				ВК-КЛОН
+			</LinkComponent>{" "}
+			{new Date().getFullYear()}.
+		</div>
+	);
 });
