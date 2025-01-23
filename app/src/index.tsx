44debb9f1e79ed8239833cee9189afb1918e21ad
Alexey Skryabin <skryabin.aleksey99@gmail.com>
2025-01-23 15:24:39 +0500

    messanger-61: test husky 10

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -8,6 +8,7 @@ import store from "@store/index";
 import "@service/i18n";
 
 import "./styles/index.scss";
+const unusedVar = 42;
 
 createRoot(document.getElementById("root") as HTMLElement)
   .render(
