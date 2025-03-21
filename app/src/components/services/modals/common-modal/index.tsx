2a51e4bb1d8f80b60d34e8262f1feeef24c872c5
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-21 13:08:58 +0600

    fix: fix modals

--- app/src/components/services/modals/common-modal/index.tsx
+++ app/src/components/services/modals/common-modal/index.tsx
@@ -1,13 +1,9 @@
 import { createPortal } from "react-dom";
-import { JSX, useEffect, useState } from "react";
-
-import Info from "@modules/carousel/info";
-import { ICarouselData } from "@components/services/modals/carousel/index";
+import { JSX } from "react";
 
 import "./common-modal.scss"
 
 interface IModalProps {
-  data: ICarouselData
   isOpen: boolean
   onClose: () => void
   children: JSX.Element;
@@ -16,39 +12,20 @@ interface IModalProps {
   description: string;
 }
 //  Основной компонент модального окна
-const CommonModal = ({
-  data,
+export default function CommonModal ({
   isOpen,
   onClose,
   children,
   className = "",
   title,
   description
-}: IModalProps) => {
+}: IModalProps) {
   if (!isOpen) return null;
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add("modal-open");
-    } else {
-      document.body.classList.remove("modal-open");
-    }
-    return () => {
-      document.body.classList.remove("modal-open");
-    };
-  }, []);
-
-  const [activeKey, setActiveKey] = useState(data.index);
-
-  const images = data.images;
-  
   return createPortal(
     <div className={`modal-overlay ${className}`} onClick={onClose} role="dialog"
       aria-labelledby={title} aria-describedby={description}
     >
-      <div className="carousel__info">
-        <Info activeImage={images[activeKey]} />
-      </div>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}
       >
         <button className="modal-close" onClick={onClose}>×</button>
@@ -59,5 +36,3 @@ const CommonModal = ({
     document.body
   )
 }
-
-export default CommonModal
