d0579eee6e13592aa6686b329c7444ab4bbeb6b7
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-02 22:51:27 +0600

    fix:(MESSANGER-87): fix Jest configs, ts config, add ts-node, sass, jest-scss-transform, @types/jest, jest-environment-jsdom and delete old tests

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -5,25 +5,24 @@ const config: Config = {
   preset: "ts-jest",
 
   // Среда для тестов, для работы с DOM компонентами
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
 
   // Массив файлов, которые будут выполнены перед тестами
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // Для настройки путей или алиасов, если они используются
   moduleNameMapper: {
     "^@components/(.*)$": "<rootDir>/src/components/$1",
   },
 
-  // Обработчик для стилей, чтобы Jest их игнорировал
   transform: {
     "^.+\\.tsx?$": "ts-jest", // Обрабатываем TypeScript файлы через ts-jest
-    "^.+\\.css$": "jest-css-modules",
+    "^.+\\.scss$": "jest-scss-transform",
   },
 
   // Настройки для покрытия (coverage) кода
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"], // собираем покрытие только для исходных файлов
+  collectCoverageFrom: ["src/components/*.{ts,tsx}", "!src/**/*.d.ts"], // собираем покрытие только для исходных файлов
 
   // Игнорируем папки и файлы, которые не должны попадать в coverage
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/", "/build/"],
