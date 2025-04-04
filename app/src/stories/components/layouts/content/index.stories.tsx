fa4335db6952f85cc90f112f320a0d3f8056f027
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-04 18:49:14 +0600

    feat(MESSANGER-87): install storybook

---
+++ app/src/stories/components/layouts/content/index.stories.tsx
@@ -0,0 +1,34 @@
+// src/components/layouts/content/index.stories.tsx
+import type { Meta, StoryObj } from "@storybook/react";
+import ContentLayout from "@components/layouts/content";
+import React from "react";
+
+const meta: Meta<typeof ContentLayout> = {
+  title: "Layouts/ContentLayout",
+  component: ContentLayout,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ContentLayout>;
+
+export const Default: Story = {
+  args: {
+    children: <div style={{ background: "#eee", padding: "20px" }}>Default content</div>,
+  },
+};
+
+export const WithLongContent: Story = {
+  args: {
+    children: (
+      <div>
+        <h3>Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+          Praesent libero. Sed cursus ante dapibus diam.
+        </p>
+      </div>
+    ),
+  },
+};
