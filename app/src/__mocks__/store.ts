a281aba2acd913c722860fb4ec1cf4ed8af5c67f
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-05-04 00:30:23 +0600

    fix(MESSENGER-89): fix jpeg buffer/sharp

--- app/src/__mocks__/store.ts
+++ app/src/__mocks__/store.ts
@@ -1,3 +1,3 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 
-export const mockStore = configureStore({ reducer: () => ({}) });
+// export const mockStore = configureStore({ reducer: () => ({}) });
